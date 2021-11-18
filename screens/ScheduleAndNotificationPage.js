63e8a9c4e30d91e58e3cc5720c58a2223e8b2be1
boss26974 <pitchaya232@gmail.com>
2021-11-18 22:52:00 +0700

    ทำ calories กับ schedules
    ทำ catagories ใน calories กับเพิ่ม calendar ใน schedules

--- screens/ScheduleAndNotificationPage.js
+++ screens/ScheduleAndNotificationPage.js
@@ -1,19 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
+import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CalendarPicker from 'react-native-calendar-picker';
 
-export default function StaticADayPage(){
-    return(
-        <View style={styles.container}>
-            <Text>Schedule And Notification</Text>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-    },
-  });
+export default class SchedulePage extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          selectedStartDate: null,
+        };
+        this.onDateChange = this.onDateChange.bind(this);
+      }
+    
+      onDateChange(date) {
+        this.setState({
+          selectedStartDate: date,
+        });
+      }
+      render() {
+        const { selectedStartDate } = this.state;
+        const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+        return (
+          <View style={styles.container}>
+            <CalendarPicker
+              onDateChange={this.onDateChange}
+            />
+    
+            <View>
+              <Text>SELECTED DATE:{ startDate }</Text>
+            </View>
+          </View>
+        );
+      }
+    }
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+      },
+    });
