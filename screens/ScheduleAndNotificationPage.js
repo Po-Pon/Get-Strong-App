2131b81fcb2530dffc6529e11d5f82d8cebc92c2
boss26974 <pitchaya232@gmail.com>
2021-11-19 22:15:15 +0700

    ทำหน้า schedule กับหน้า staticADay เพิ่มเติม
    หน้า schedule มีบอกวันที่เลือกจากปฏิทิน หน้า Static Aday เพิ่มกรอบแสดงเวลา กับ progress ในแต่ละวัน

--- screens/ScheduleAndNotificationPage.js
+++ screens/ScheduleAndNotificationPage.js
@@ -1,43 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import CalendarPicker from 'react-native-calendar-picker';
+import { format } from 'date-fns';
 
-export default class SchedulePage extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-          selectedStartDate: null,
-        };
-        this.onDateChange = this.onDateChange.bind(this);
-      }
-    
-      onDateChange(date) {
-        this.setState({
-          selectedStartDate: date,
-        });
-      }
-      render() {
-        const { selectedStartDate } = this.state;
-        const startDate = selectedStartDate ? selectedStartDate.toString() : '';
-        return (
-          <View style={styles.container}>
-            <CalendarPicker
-              onDateChange={this.onDateChange}
-            />
-    
-            <View>
-              <Text>SELECTED DATE:{ startDate }</Text>
-            </View>
+export default function SchedulePage({ navigation }){
+  const [selectDate, setSelectDate] = useState("")
+
+    return(
+        <View>
+          <CalendarPicker 
+          onDateChange={(date) => {
+            let newDate = date.toDate()
+            let showDate = format(newDate, "MMMM dd, yyyy")
+            setSelectDate(showDate)
+          }}
+          todayBackgroundColor="yellow"
+          ></CalendarPicker>
+          <View pointerEvents="auto" style={styles.selectdaySchedules}>
+            <Text style={styles.textDate}>{selectDate}</Text>
+            <Text style={styles.textSchedule}>Your Schedules</Text>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {navigation.navigate('Static Today')}}
+            >
+            <Text>Watch Your Static</Text>
+            </TouchableOpacity>
           </View>
-        );
-      }
-    }
+        </View>
+    )
+}
     
     const styles = StyleSheet.create({
       container: {
         flex: 1,
         backgroundColor: '#FFFFFF',
+        alignItems: 'center'
       },
+      selectdaySchedules: {
+        marginTop: 50,
+        width: '85%',
+        alignSelf: 'center',
+        borderRadius: 10,
+        borderColor: 'black',
+        borderWidth: 4,
+      },
+      textDate: {
+        textAlign: 'center', 
+        fontSize: 24
+      },
+      textSchedule: {
+        textAlign: 'left', 
+        fontSize: 18, 
+        marginTop: 5
+      },
+      button: {
+        alignItems: "center",
+        backgroundColor: "#ff0e00",
+        padding: 10
+      }
     });
--- screens/StaticInADayPage.js
+++ screens/StaticInADayPage.js
@@ -1,19 +1,115 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import moment from 'moment';
 
 export default function StaticADayPage(){
-    return(
-        <View style={styles.container}>
-            <Text>Static A Day</Text>
+    const [timeNow, setTimeNow] = useState(new moment())
+    const [calories, setCalories] = useState(0)
+    const [dayProgress, setDayProgress] = useState(0.1)
+
+    const refreshClock = () => {
+        setTimeNow(new moment());
+      }
+    
+    const progressValue = {
+      borderColor: 'red',
+      borderWidth: 1,
+      height: '100%',
+      backgroundColor: 'red',
+      width: dayProgress*100 + "%",
+      alignSelf: 'flex-start'
+    }
+
+      useEffect(() => {
+        const timerId = setInterval(refreshClock, 1000);
+        return function cleanup() {
+            clearInterval(timerId);
+          };
+      }, []);
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.time}>
+            <Text style={styles.textTime}>{timeNow.format('LT')}</Text>
+            <Text style={styles.textDate}>{timeNow.format('MM/D/YYYY')}</Text>
         </View>
+        <View style={styles.calender}>
+
+        </View>
+        <View style={styles.progress}>
+            <Text style={{top: 5}}>เป้าหมายของวันนี้</Text>
+            <View style={styles.progressBar}>
+              <View style={progressValue}/>
+            </View>
+        </View> 
+        <View style={styles.compare}></View>
+      </View>
     )
+
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
+      justifyContent: 'flex-start',
+      alignItems: 'center'
     },
+    time: {
+      marginTop: 30,
+      borderColor: 'black',
+      borderWidth: 4,
+      width: '90%',
+      height: '30%',
+      alignItems: 'center'
+    },
+    textTime: {
+      fontSize: 72,
+    },
+    textDate: {
+      fontSize: 24,
+      alignSelf: 'flex-end',
+      top: 30
+    },
+    calender: {
+      marginTop: 20,
+      borderColor: 'black',
+      borderWidth: 4,
+      width: '90%',
+      height: '15%',
+      alignItems: 'center'
+    },
+    progress: {
+      marginTop: 20,
+      borderColor: 'black',
+      borderWidth: 4,
+      width: '90%',
+      height: '15%',
+      alignItems: 'center'
+    },
+    progressBar: {
+      marginTop: 5,
+      borderColor: 'black',
+      borderWidth: 4,
+      width: '90%',
+      height: '50%',
+      alignItems: 'center'
+    },
+    progressValue: {
+      borderColor: 'red',
+      borderWidth: 1,
+      height: '100%',
+      backgroundColor: 'red',
+      width: '100%',
+      alignSelf: 'flex-start'
+    },
+    compare: {
+      marginTop: 20,
+      borderColor: 'black',
+      borderWidth: 4,
+      width: '90%',
+      height: '20%',
+      alignItems: 'center'
+    }
   });
